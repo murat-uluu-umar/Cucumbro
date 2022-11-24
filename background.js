f174c83f265f64a89005557983e8af6bcc8795be
Voyager <umbn614@gmail.com>
2022-11-24 17:54:06 +0600

    stopwatch and countdown end #1

--- background.js
+++ background.js
@@ -1,15 +1,72 @@
-const START = 'START'
-const STOPWATCH = 'STOPWATCH'
-const COUNTDOWN = 'COUNTDOWN'
+const START = "START";
+const STOPWATCH = "STOPWATCH";
+const COUNTDOWN = "COUNTDOWN";
 
-var stopwatch_date = Date()
-var countdown_date = Date()
+var stopwatch_date = Date();
+var countdown_date = Date();
 
-var state = START
+var state = START;
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msg === 'getState')
-            sendResponse(state)
+// notificaton
+function notification() {
+  console.log("Do your job!");
+}
+
+// stopwatch
+var stopwatchDelay = 30;
+var interval = null;
+var delay = 1000;
+var restTime = 0;
+
+// countdown
+const ALARM = "countdown";
+function countdownInit() {
+  chrome.alarms.get(ALARM, (alarm) => {
+    if (!alarm) {
+      chrome.alarms.create(ALARM, { delayInMinutes: restTime / 60 });
+    } else {
+      var distance = alarm.scheduledTime - new Date().getTime();
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      result = { minutes: minutes, seconds: seconds };
+      chrome.runtime.sendMessage({ msg: "tick", value: result }, (response) => {});
     }
-  )
+  });
+}
+
+// message handler
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  switch (request.msg) {
+    case "getState":
+      sendResponse(state);
+      break;
+    case "setState":
+      state = request.value;
+      break;
+    case "stopwatchTick":
+      if (interval === null) {
+        interval = setInterval(() => {
+          stopwatchDelay++;
+        }, delay);
+      }
+      sendResponse(stopwatchDelay);
+      break;
+    case "stopwatchEnd":
+      if (interval !== null) clearInterval(interval);
+      restTime = stopwatchDelay / 3;
+      console.log(restTime);
+      console.log(stopwatchDelay);
+      stopwatchDelay = 0;
+      break;
+    case "countdownInit":
+      countdownInit(request.value);
+  }
+});
+
+// alarm
+chrome.alarms.onAlarm.addListener((alarm) => {
+  restTime = 0;
+  chrome.runtime.sendMessage({ msg: "countdownEnd" }, (response) => {});
+  chrome.alarms.clearAll();
+  notification();
+});
--- popup.js
+++ popup.js
@@ -2,16 +2,62 @@ const START = "START";
 const STOPWATCH = "STOPWATCH";
 const COUNTDOWN = "COUNTDOWN";
 
-var type = document.getElementById("type");
 var startPanel = document.getElementById("start");
 var stopwatchPanel = document.getElementById("stopwatch");
 var countdownPanel = document.getElementById("countdown");
 
 // start
-var startBtn = document.getElementById("startBtn")
+var startBtn = document.getElementById("startBtn");
 
+// stopwatch
+var mins = document.getElementById("tens");
+var seconds = document.getElementById("seconds");
+var restBtn = document.getElementById("restBtn");
 
-var stopwatch = "⏱️";
+// countdown
+var cmins = document.getElementById("mins");
+var cseconds = document.getElementById("secs");
+var skipBtn = document.getElementById("skipBtn");
+
+// objects
+
+const stopwatch = {
+  delay: 1000,
+  interval: null,
+  update: () => {
+    interval = setInterval(() => {
+      chrome.runtime.sendMessage({ msg: "stopwatchTick" }, (time) => {
+        mins.innerHTML = digits(Math.floor(time / 60));
+        seconds.innerHTML = digits(time % 60);
+      });
+    }, this.delay);
+  },
+  end: () => {
+    if (this.interval !== null) {
+      clearInterval(this.interval);
+      chrome.runtime.sendMessage({ msg: "stopwatchEnd" }, (time) => {});
+    }
+  },
+};
+
+const countdown = {
+  delay: 3000,
+  interval: null,
+  update: () => {
+    interval = setInterval(() => {
+      chrome.runtime.sendMessage(
+        { msg: "countdownInit", value: updateDigits },
+        (time) => {}
+      );
+    }, this.delay);
+  },
+  end: () => {
+    if (this.interval !== null) {
+      clearInterval(this.interval);
+      changeState(START);
+    }
+  },
+};
 
 window.onload = () => {
   updatePanel();
@@ -23,12 +69,21 @@ function updatePanel() {
     switch (response) {
       case START:
         startPanel.style.display = "block";
+        startBtn.onclick = () => changeState(STOPWATCH);
         break;
       case STOPWATCH:
         stopwatchPanel.style.display = "block";
+        stopwatch.update();
+        restBtn.onclick = () => {
+          stopwatch.end();
+          changeState(COUNTDOWN);
+        };
         break;
       case COUNTDOWN:
         countdownPanel.style.display = "block";
+        countdown.update();
+        tickHandler()
+        countdownEndHandler();
         break;
     }
   });
@@ -38,4 +93,40 @@ function hideAll() {
   startPanel.style.display = "none";
   stopwatchPanel.style.display = "none";
   countdownPanel.style.display = "none";
+}
+
+function digits(num) {
+  num = num.toString();
+  while (num.length < 2) num = "0" + num;
+  return num;
+}
+
+function updateDigits(time) {
+  cmins.innerHTML = digits(time.minutes);
+  cseconds.innerHTML = digits(time.seconds);
+}
+
+async function changeState(state) {
+  await chrome.runtime.sendMessage(
+    { msg: "setState", value: state },
+    (response) => {}
+  );
+  updatePanel();
+}
+
+function countdownEndHandler() {
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.msg === "countdownEnd") countdown.end();
+  });
+}
+
+function tickHandler() {
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.msg == 'tick')
+      updateDigits(request.value)
+  });
 }
